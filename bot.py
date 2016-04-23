f97b1680f281007c7ab2ae1ebb571f94c0045b5c
Tosh Camille <tochecamille@gmail.com>
2016-04-23 13:05:43 +0200

    Add English documentation

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import http.client
 
 
 def download(mention):
+    # Call api with Twitter name to create image
 
     user = mention.user.screen_name
     conn = http.client.HTTPConnection("localhost")
@@ -14,27 +15,32 @@ def download(mention):
 
 
 def reply(api, mention):
+    # Reply to the tweet who mention the bot with image saved in tmp/<username>.jpg
 
     id = mention.id
     user = mention.user.screen_name
 
     img = "tmp/" + user + ".jpg"
     print(img)
+    # For documentation view Twitter API's documentation
     api.update_with_media(img, "Be like Bill, proudly, @" + user, id)
 
 
 if __name__ == '__main__':
+    # You can add your Twitter Secret Application keys here. Go to https://dev.twitter.com
 
     consumer_key = ''
     consumer_secret = ''
     access_token = ''
     access_secret = ''
 
+    # Use Oauth to connect application
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
 
     api = tweepy.API(auth)
 
+    # Get the last mention
     mentions = api.mentions_timeline(count=1)
 
     for mention in mentions:
--- readme.md
+++ readme.md
@@ -1,9 +1,25 @@
-# Be like Bill Twitter bot
+# Be like Bill Twitter bot auto-reply
 
-J'ai utilisé cette [api](https://github.com/gautamkrishnar/Be-Like-Bill).
+## Utils
 
-Le bot utilise :
+The modification's are : 
+
+- Change image name with Twitter's account name.
+- Change destination folder
+- remove *useless* files, files doesn't used by the API
+
+
+The bot use :
 
 - tweepy
 - http.client
 
+You can install require lib's with ```pip install <nameOfLib>```
+
+For start the bot you can use ```python bot.py``` command.
+
+## Test
+
+You can add [@BeLikeBillBot](https://twitter.com/BeLikeBillBot) when you post a tweet and the Bot (when is started) reply to your tweet with "Be Like *your Twitter name*" image.
+
+I use and modiy this [api](https://github.com/gautamkrishnar/Be-Like-Bill). 
